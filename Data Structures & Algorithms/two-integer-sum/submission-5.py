72177269f007aaacc048ab5732ab9165beeccda4
Samar Patil <64025321+Samar1999@users.noreply.github.com>
2026-06-25 20:05:36 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        dict1 = {}
+        
+        for i in range(0,len(nums)):
+            diff  = target - nums[i]
+            if diff not in dict1:
+                dict1[nums[i]] = i
+            else:
+                return [min(dict1[diff], i), max(dict1[diff], i)]
+        
+            
+
+
+
